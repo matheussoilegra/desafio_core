6fcb7abf6ad0fb84bc87a8bf1c3dfaf51da67018
Matheus Santos <matheus.santos@ilegra.com>
2019-07-29 14:42:37 -0300

    Ajustados espaços.

--- src/main/java/persistence/Writer.java
+++ src/main/java/persistence/Writer.java
@@ -23,14 +23,14 @@ public class Writer {
         try {
 //            FileWriter arquivo = new FileWriter(new File("C:\\Users\\mathe\\Desktop\\desafiocorefiles\\data\\out\\file2.done.dat"));
             FileWriter arquivo = new FileWriter(new File("/home/ilegra0369/Downloads/desafiocorefiles/data/out/file2.done.dat"));
-            arquivo.write("Quantidade de clientes: " + clienteGerenciamento.totalClientes() + "\r\n"
-                    + "Quantidade de vendedores: " + vendedorGerenciamento.totalVendedores() + "\r\n"
-                    + "Id da venda mais cara: " + vendasGerenciamento.getIdVendaCara() + "\r\n"
+            arquivo.write("Quantidade de clientes: " + clienteGerenciamento.totalClientes() + "\n"
+                    + "Quantidade de vendedores: " + vendedorGerenciamento.totalVendedores() + "\n"
+                    + "Id da venda mais cara: " + vendasGerenciamento.getIdVendaCara() + "\n"
                     + "Pior Vendedor: " + vendasGerenciamento.getPiorVenda());
             arquivo.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+
 }
